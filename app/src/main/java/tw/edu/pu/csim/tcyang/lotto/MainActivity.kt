ed556c7b5a11546ac926d65cc1b349aaf06a9954
li <alice2005921@gmail.com>
2025-09-22 15:27:30 +0800

    第七次commit (螢幕觸控)(李羿慧)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -1,9 +1,11 @@
 package tw.edu.pu.csim.tcyang.lotto
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
 
 import androidx.compose.runtime.setValue // 引入 setValue
+import androidx.compose.ui.platform.LocalContext
 
 
 class MainActivity : ComponentActivity() {
@@ -45,8 +48,13 @@ fun Play(modifier: Modifier = Modifier) {
     var lucky by remember {
         mutableStateOf((1..100).random())
     }
+    val context = LocalContext.current
 
-    Column (modifier = modifier.fillMaxSize(),
+    Column (modifier = modifier
+        .fillMaxSize()
+        .clickable {
+            Toast.makeText(context, "螢幕觸控(李羿慧)", Toast.LENGTH_SHORT).show()
+        },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
         ){
